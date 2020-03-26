6de2341fe9d44ee82df749b41df626dc9e63dffd
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2020-03-27 00:06:48 +0530

    Update code.c
    made few things global
    took out few funtions to make code a bit clean
    try to keep main() as clean as possible

--- code.c
+++ code.c
@@ -1,102 +1,121 @@
-#include<stdio.h> 
- 
-#define N 10 
- 
-typedef struct 
-{ 
+#include<stdio.h>
+
+#define N 10
+int count;
+typedef struct
+{
       int process_id, arrival_time, burst_time, priority;
-      int q, ready; 
-}process_structure; 
- 
-int Queue(int t1) 
-{ 
-      if(t1 == 0 || t1 == 1 || t1 == 2 || t1 == 3) 
-      { 
-            return 1; 
-      } 
+      int q, ready;
+}process_structure process[10]; //made process queue global
+
+int assignQueue(int t1)
+{
+      if(t1 <= 3) //changed condtion
+      {
+            return 1;
+      }
       else
       {
-            return 2; 
+            return 2;
       }
-} 
- 
-int main() 
-{ 
-      int limit, count, temp_process, time, j, y; 
-      process_structure temp; 
-      printf("Enter Total Number of Processes:\t"); 
-      scanf("%d", &limit);  
-      process_structure process[limit]; 
-      for(count = 0; count < limit; count++) 
-      { 
-            printf("\nProcess ID:\t"); 
-            scanf("%d", &process[count].process_id); 
-            printf("Arrival Time:\t"); 
-            scanf("%d", &process[count].arrival_time); 
-            printf("Burst Time:\t"); 
-            scanf("%d", &process[count].burst_time); 
-            printf("Process Priority:\t"); 
-            scanf("%d", &process[count].priority); 
-            temp_process = process[count].priority; 
-            process[count].q = Queue(temp_process);
-            process[count].ready = 0; 
+}
+void ready(int y)
+{
+  for(index = y; index < limit; index++)
+  {
+        if(process[index].arrival_time < time)
+        {
+              process[index].ready = 1;
+        }
+  }
+}
+
+void sort_by_qt(int y)
+{ process temp;
+  for(index = y; index < limit - 1; index++)
+  {
+        for(j = index + 1; j < limit; j++)
+        {
+              if(process[index].ready == 1 && process[j].ready == 1)
+              {
+                    if(process[index].q == 2 && process[j].q == 1)
+                    {
+                          temp = process[index];
+                          process[index] = process[j];
+                          process[j] = temp;
+                    }
+              }
+        }
+  }
+}
+
+sort_by_bt(int y)
+{process temp;
+  for(index = y; index < limit - 1; index++)
+  {
+        for(j = index + 1; j < limit; j++)
+        {
+              if(process[index].ready == 1 && process[j].ready == 1)
+              {
+                    if(process[index].q == 1 && process[j].q == 1)
+                    {
+                          if(process[index].burst_time > process[j].burst_time)
+                          {
+                                temp = process[index];
+                                process[index] = process[j];
+                                process[j] = temp;
+                          }
+                          else
+                          {
+                                break;
+                          }
+                    }
+              }
+        }
+  }
+}
+int main()
+{
+      int  index, temp_process, time, j, y; //made limit global
+      process_structure temp;
+      printf("Enter Total Number of Processes:\t");
+      scanf("%d", &limit);
+      //process_structure process[limit];
+      for(index = 0; index < limit; index++)
+      {
+            printf("\nProcess ID:\t");
+            scanf("%d", &process[index].process_id);
+            printf("Arrival Time:\t");
+            scanf("%d", &process[index].arrival_time);
+            printf("Burst Time:\t");
+            scanf("%d", &process[index].burst_time);
+            printf("Process Priority:\t");
+            scanf("%d", &process[index].priority);
+            //avoided the temp_process variable
+            process[index].q = assignQueue(process[index].priority);
+            process[index].ready = 0;
       }
-      time = process[0].burst_time; 
-      for(y = 0; y < limit; y++) 
-      { 
-            for(count = y; count < limit; count++) 
-            { 
-                  if(process[count].arrival_time < time) 
-                  {
-                        process[count].ready = 1; 
-                  } 
-            } 
-            for(count = y; count < limit - 1; count++) 
+      time = process[0].burst_time;
+      for(y = 0; y < limit; y++)
+      {
+            //ready function
+            ready(y);
+
+            //sort by qt func
+            sort_by_qt(y);
+
+            //sort by bt func
+            sort_by_bt(y);
+
+            printf("\nProcess[%d]:\tTime:\t%d To %d\n", process[y].process_id, time, time + process[y].burst_time);
+            time = time + process[y].burst_time;
+            for(index = y; index < limit; index++)
             {
-                  for(j = count + 1; j < limit; j++) 
-                  { 
-                        if(process[count].ready == 1 && process[j].ready == 1) 
-                        { 
-                              if(process[count].q == 2 && process[j].q == 1) 
-                              { 
-                                    temp = process[count]; 
-                                    process[count] = process[j]; 
-                                    process[j] = temp; 
-                              } 
-                        } 
-                  } 
-            } 
-            for(count = y; count < limit - 1; count++) 
-            { 
-                  for(j = count + 1; j < limit; j++) 
+                  if(process[index].ready == 1)
                   {
-                        if(process[count].ready == 1 && process[j].ready == 1) 
-                        { 
-                              if(process[count].q == 1 && process[j].q == 1) 
-                              { 
-                                    if(process[count].burst_time > process[j].burst_time) 
-                                    { 
-                                          temp = process[count]; 
-                                          process[count] = process[j]; 
-                                          process[j] = temp; 
-                                    } 
-                                    else 
-                                    { 
-                                          break; 
-                                    } 
-                              } 
-                        } 
-                  } 
-            } 
-            printf("\nProcess[%d]:\tTime:\t%d To %d\n", process[y].process_id, time, time + process[y].burst_time); 
-            time = time + process[y].burst_time; 
-            for(count = y; count < limit; count++) 
-            { 
-                  if(process[count].ready == 1) 
-                  { 
-                        process[count].ready = 0; 
-                  } 
-            } 
-      } 
-      return 0; 
+                        process[index].ready = 0;
+                  }
+            }
+      }
+      return 0;
 }
